05a8ec5763a069ce4a0ca1138bd2593dabe56478
Onurcan Erşen <ocersen@gmail.com>
2022-12-24 00:51:07 +0300

    Add modal

--- lib/Screens/Login/LoginLayout.dart
+++ lib/Screens/Login/LoginLayout.dart
@@ -169,7 +169,29 @@ class _LoginLayoutState extends State<LoginLayout> {
                                     ),
                                   );
                                 } else {
-                                  print("tamamdir degildir hojam");
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return Dialog(
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(40)),
+                                        elevation: 16,
+                                        child: Container(
+                                          child: ListView(
+                                            shrinkWrap: true,
+                                            children: <Widget>[
+                                              SizedBox(height: 20),
+                                              Center(
+                                                  child: Text(
+                                                      'Invalid email or password')),
+                                              SizedBox(height: 20),
+                                            ],
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                  );
                                 }
                               }
                             },
